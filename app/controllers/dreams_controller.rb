45349f953c75d37c5c609ecea66590d60101e625
Julien Discrit <juliendiscrit@gmail.com>
2023-03-07 13:49:37 +0100

    ajout méthode new et create pour dream

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,2 +1,21 @@
 class DreamsController < ApplicationController
+  def new
+    @dream = Dream.new
+  end
+
+  def create
+    @dream = Dream.new(dream_params)
+    @dream.user = current_user
+    if @dream.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity, alert: "Please try again"
+    end
+  end
+
+  private
+
+  def dream_params
+    params.require(:dream).permit(:content, :character, :ambiance, :location)
+  end
 end
