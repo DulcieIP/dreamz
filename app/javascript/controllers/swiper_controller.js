6ec0acd381860bbcd0bd77d8376ec4cebca92131
Mathieu Zimeris <alexvargas@hotmail.fr>
2023-03-09 14:58:58 +0100

    voilà pour un début

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -1,31 +1,14 @@
-// app/javascript/controllers/swiper_controller.js
+import { Controller } from "@hotwired/stimulus"
+import Swiper from "swiper";
+import 'swiper/css';
 
-  // core version + navigation, pagination modules:
-  import Swiper, { Navigation, Pagination } from 'swiper';
-  // import Swiper and modules styles
-  import 'swiper/css';
-  import 'swiper/css/navigation';
-  import 'swiper/css/pagination';
-
-  // init Swiper:
-  const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+export default class extends Controller {
+  connect() {
+    console.log("swiper controller connecté")
+    new Swiper(this.element, {
+      direction: "horizontal",
+      loop: true,
+      // Swiper options go here
+    });
+  }
+}
