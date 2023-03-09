af025f65b7e02521c056ac0939445fe59aae31f9
Mathieu Zimeris <alexvargas@hotmail.fr>
2023-03-09 11:32:45 +0100

    poubelle

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+// app/javascript/controllers/swiper.js
+
+    // import Swiper JS
+    import Swiper from 'swiper';
+    // import Swiper styles
+    import 'swiper/css';
