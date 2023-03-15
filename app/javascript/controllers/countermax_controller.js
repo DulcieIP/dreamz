44a8e8bfcd4a643b1e1c4ddd37fd4ef226d8fadb
Julien Discrit <juliendiscrit@gmail.com>
2023-03-15 13:18:42 +0100

    pulse bouton

--- app/javascript/controllers/countermax_controller.js
+++ app/javascript/controllers/countermax_controller.js
@@ -5,14 +5,12 @@ export default class extends Controller {
   static targets = ["counter", "input"]
 
   check() {
-   
+
     if (this.inputTarget.value.length > 250){
       this.counterTarget.classList.add("red")
     } else {
       this.counterTarget.classList.remove("red")
     }
-
-
   }
 
 }
