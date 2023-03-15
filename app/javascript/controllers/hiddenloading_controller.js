de11e5c42e7e0f5aa907b75cc992cf51ba1a778e
Mathieu Zimeris <alexvargas@hotmail.fr>
2023-03-15 12:30:05 +0100

    correction de la new pour que la loading page se charge bien

--- app/javascript/controllers/hiddenloading_controller.js
+++ app/javascript/controllers/hiddenloading_controller.js
@@ -4,7 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["loading", "form"]
 
-
   show() {
     this.loadingTarget.classList.remove("d-none")
     this.formTarget.classList.add("d-none")
