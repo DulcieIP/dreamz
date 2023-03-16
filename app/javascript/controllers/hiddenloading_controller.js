f443a3f0cd546604eebb6e5925b938f938fc30f6
Dulcie IRVANI <dulcie.irvani@gmail.com>
2023-03-16 12:35:59 +0100

    adding text to loading page

--- app/javascript/controllers/hiddenloading_controller.js
+++ app/javascript/controllers/hiddenloading_controller.js
@@ -1,11 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
 
 // Connects to data-controller="hiddenloading"
 export default class extends Controller {
-  static targets = ["loading", "form"]
+  static targets = ["loading", "form", "text"]
 
   show() {
     this.loadingTarget.classList.remove("d-none")
     this.formTarget.classList.add("d-none")
+
+    new Typed(this.textTarget, {
+      strings: ['Dream in progress...', 'Compiling your brain cells...', 'Get ready to meet your subconscious...', 'Almost there...', "What's a few seconds of waiting compared to the 6 years you'll spend dreaming?", 'Bringing your dreams to reality...', "Did you know that 90% of dreams are forgotten within 10 minutes of waking up?" ],
+      typeSpeed: 90,
+      loop: true,
+    })
   }
 }
